41ef7dff529e447b8f7f72449ba2657b2abc03a5
komal-habura <64227198+komal-habura@users.noreply.github.com>
2020-08-21 22:33:07 +0530

    Update key.tf

--- key.tf
+++ key.tf
@@ -57,22 +57,3 @@ resource "aws_security_group" "mysecurity1" {
   }
 }
 
-resource "aws_efs_file_system" "myefs" {
-  creation_token = "myefs"
-  encrypted = "true"
-
-  tags = {
-    Name = "myefs"
-  }
-  lifecycle_policy {
-    transition_to_ia = "AFTER_30_DAYS"
-  }
-}
-resource "aws_vpc" "myefs" {
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_efs_mount_target" "myefs" {
-  file_system_id = "${aws_efs_file_system.myefs.id}"
-  subnet_id      = "subnet-c4600b88"
-}
-
